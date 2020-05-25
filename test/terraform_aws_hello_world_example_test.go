e208cf9ae04af9d9422bff93a6cdd0b3a5c78d75
Douglas Naphas <douglasnaphas@gmail.com>
2020-05-25 18:17:39 -0400

    Verify that test resources are created

--- test/terraform_aws_hello_world_example_test.go
+++ test/terraform_aws_hello_world_example_test.go
@@ -6,8 +6,8 @@ import (
 	"time"
 
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAwsHelloWorldExample(t *testing.T) {
@@ -30,4 +30,9 @@ func TestTerraformAwsHelloWorldExample(t *testing.T) {
 	// website::tag::4:: Make an HTTP request to the instance and make sure we get back a 200 OK with the body "Hello, World!"
 	url := fmt.Sprintf("http://%s:8080", publicIp)
 	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
+	s, d := http_helper.HttpGet(t, url, nil)
+	fmt.Println("received:")
+	fmt.Println(s)
+	fmt.Println(d)
+	assert.Equal(t, "Hello, World!", d)
 }
--- test/terraform_hello_world_example_test.go
+++ test/terraform_hello_world_example_test.go
@@ -1,10 +1,14 @@
 package test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 )
 
 func TestTerraformHelloWorldExample(t *testing.T) {
@@ -22,4 +26,17 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	// website::tag::3:: Run `terraform output` to get the values of output variables and check they have the expected values.
 	output := terraform.Output(t, terraformOptions, "hello_world")
 	assert.Equal(t, "Hello, World!", output)
+	url := "https://jsonplaceholder.typicode.com/posts/1"
+	data :=
+		`{
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}`
+	s, d := http_helper.HttpGet(t, url, nil)
+	fmt.Println(s)
+	fmt.Println(d)
+	assert.Equal(t, data, d)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, data, 1, 5*time.Second)
 }
